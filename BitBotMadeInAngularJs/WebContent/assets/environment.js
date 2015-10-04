7b4d796e60f67db74d83f4c673c667605b4b8190
paologiranda <paologiranda@gmail.com>
2015-10-04 10:39:59 +0200

    Integrazione rootScope tra pagine carrello e login.

--- BitBotMadeInAngularJs/WebContent/assets/environment.js
+++ BitBotMadeInAngularJs/WebContent/assets/environment.js
@@ -1,4 +1,5 @@
 angular.module('bitBotApp').value('API_CONF',{
 	base_url: '/rest',
-	server: 'http://localhost:8081',//punto all'indirizzo del mio pc		
+	server: 'http://localhost:8081',//punto all'indirizzo del mio pc
+//	server: 'http://10.174.54.246:8081/'// ip dep mio pc
 });
