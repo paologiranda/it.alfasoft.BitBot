ec1a2b8bee7627bc097259c6bbeb13f0094754ba
paologiranda <paologiranda@gmail.com>
2015-10-12 21:34:57 +0200

    provola

--- bitbot/src/it/alfasoft/ecommerce/login/LoginTest.java
+++ bitbot/src/it/alfasoft/ecommerce/login/LoginTest.java
@@ -24,68 +24,77 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LoginTest {
 	Login lg = new Login();
+	
 	private GestioneAdmin ga = new GestioneAdmin();
 	ApplicationContext ctx = new ClassPathXmlApplicationContext(
 			new String[] { "clientiBo.xml" });
 	GestioneClienti gc = ctx.getBean("clienti", GestioneClienti.class);
 
+//	@Test
+//	public void loginClienteEsistenteTest() throws ClassNotFoundException,
+//			ErroreSistema, TabellaNonTrovata, SQLException {
+//
+//		String url = "jdbc:oracle:thin:@//localhost:1521/XE";
+//		IDaoGestioneClienti dao = new DaoClienti(url);
+//
+//		try {
+//			gc.aggiungiCliente("rag", "11216", CategoriaCliente.Azienda,
+//					"01103253", "ciao@ciao.it", "Italia", "Torino", "TO", 3,
+//					"B", CategoriaVia.via, "Tripoli", 10100, "9", "ciao");
+//		} catch (ClienteGiaRegistrato | PivaGiaRegistrata e) {
+//
+//		}
+//
+//		try {
+//			Assert.assertNotNull(lg.login("ciao@ciao.it", "ciao"));
+//		} catch (LoginErrato le) {
+//			Assert.assertTrue(false);
+//		} catch (ClienteNonattivo e) {
+//
+//		}
+//		dao.eliminaCliente("ciao@ciao.it");
+//	}
+//
+//	@Test
+//	public void loginAdminEsistenteTest() throws LoginErrato, ErroreSistema,
+//			ClienteNonattivo {
+//		Admin adm = new Admin("luca", "salzone", "luca",
+//				"luca.salzone@gmail.com");
+//		try {
+//			ga.registrazioneAdmin(adm);
+//		} catch (UtenteGiaEsistente e) {
+//
+//		}
+//		Assert.assertNotNull(lg.login("luca.salzone@gmail.com", "luca"));
+//	}
+//
+//	@Test
+//	public void loginClienteNonEsistenteTest() throws ErroreSistema,
+//			ClienteNonattivo {
+//		try {
+//			lg.login("abvdulah", "ghirigori");
+//			fail("Doveva lanciare eccezione LoginErrato");
+//		} catch (LoginErrato le) {
+//			Assert.assertTrue(true);
+//		}
+//	}
+//
+//	@Test
+//	public void loginAdminNonEsistenteTest() throws ErroreSistema,
+//			ClienteNonattivo {
+//		try {
+//			lg.login("abvdulah", "ghirigori");
+//			fail("Doveva lanciare eccezione LoginErrato");
+//		} catch (LoginErrato le) {
+//			Assert.assertTrue(true);
+//		}
+//	}
 	@Test
-	public void loginClienteEsistenteTest() throws ClassNotFoundException,
-			ErroreSistema, TabellaNonTrovata, SQLException {
-
-		String url = "jdbc:oracle:thin:@//localhost:1521/XE";
-		IDaoGestioneClienti dao = new DaoClienti(url);
-
-		try {
-			gc.aggiungiCliente("rag", "11216", CategoriaCliente.Azienda,
-					"01103253", "ciao@ciao.it", "Italia", "Torino", "TO", 3,
-					"B", CategoriaVia.via, "Tripoli", 10100, "9", "ciao");
-		} catch (ClienteGiaRegistrato | PivaGiaRegistrata e) {
-
-		}
-
-		try {
-			Assert.assertNotNull(lg.login("ciao@ciao.it", "ciao"));
-		} catch (LoginErrato le) {
-			Assert.assertTrue(false);
-		} catch (ClienteNonattivo e) {
-
-		}
-		dao.eliminaCliente("ciao@ciao.it");
+	public void searchDataForAzienda() throws ErroreSistema,
+			ClienteNonattivo, ClassNotFoundException, SQLException {
+		DaoLogin dao = new DaoLogin();
+		String mail = "azienda@gmail.com";
+		Assert.assertNotNull(dao.searchDataForAzienda(mail));
 	}
-
-	@Test
-	public void loginAdminEsistenteTest() throws LoginErrato, ErroreSistema,
-			ClienteNonattivo {
-		Admin adm = new Admin("luca", "salzone", "luca",
-				"luca.salzone@gmail.com");
-		try {
-			ga.registrazioneAdmin(adm);
-		} catch (UtenteGiaEsistente e) {
-
-		}
-		Assert.assertNotNull(lg.login("luca.salzone@gmail.com", "luca"));
-	}
-
-	@Test
-	public void loginClienteNonEsistenteTest() throws ErroreSistema,
-			ClienteNonattivo {
-		try {
-			lg.login("abvdulah", "ghirigori");
-			fail("Doveva lanciare eccezione LoginErrato");
-		} catch (LoginErrato le) {
-			Assert.assertTrue(true);
-		}
-	}
-
-	@Test
-	public void loginAdminNonEsistenteTest() throws ErroreSistema,
-			ClienteNonattivo {
-		try {
-			lg.login("abvdulah", "ghirigori");
-			fail("Doveva lanciare eccezione LoginErrato");
-		} catch (LoginErrato le) {
-			Assert.assertTrue(true);
-		}
-	}
+	
 }
