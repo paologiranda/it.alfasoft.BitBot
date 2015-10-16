fd2d2f32be6bb5ed6b6690eabeee35e7af5cc9f3
paologiranda <paologiranda@gmail.com>
2015-10-16 19:13:06 +0200

    modifica modulo BitBotApp in app

--- BitBotMadeInAngularJs/WebContent/assets/environment.js
+++ BitBotMadeInAngularJs/WebContent/assets/environment.js
@@ -1,5 +1,5 @@
-angular.module('bitBotApp').value('API_CONF',{
+angular.module('app').value('API_CONF',{
 	base_url: '/rest',
-	server: 'http://localhost:8081',//punto all'indirizzo del mio pc
+	server: 'http://localhost:9000',//punto all'indirizzo del mio pc
 //	server: 'http://10.174.54.246:8081/'// ip dep mio pc
 });
